c59c7c7fcd5b8a1a91807b3bddfd2d7afa8ec3ab
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-09-06 15:49:51 +0300

    05-What's In A Field?

--- app/Nova/Book.php
+++ app/Nova/Book.php
@@ -46,11 +46,19 @@ class Book extends Resource
         return [
             ID::make()->sortable(),
 
-            Text::make('Title'),
+            Text::make('Title')
+                ->sortable()
+                ->required(),
 
-            Number::make('Pages', 'number_of_pages'),
+            Number::make('Pages', 'number_of_pages')
+                ->filterable()
+                ->hideFromIndex()
+                ->required(),
 
-            Number::make('Copies', 'number_of_copies'),
+            Number::make('Copies', 'number_of_copies')
+                ->sortable()
+                ->required()
+                ->help('The total number of copies of this book that the library owns.'),
         ];
     }
 
