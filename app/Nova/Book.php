86584b3b5446364c6d2d811d6338ba521e08cba8
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-09-22 09:47:32 +0300

    13-Search And Scout (part 01) (vi)

--- app/Nova/Book.php
+++ app/Nova/Book.php
@@ -14,6 +14,7 @@ use Laravel\Nova\Fields\FormData;
 use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Image;
+use Laravel\Nova\Fields\MorphMany;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Trix;
@@ -131,6 +132,8 @@ class Book extends Resource
 
             BelongsToMany::make('Current Loans', resource: Customer::class)
                 ->fields(new LoanFields()),
+
+            MorphMany::make('Reviews'),
         ];
     }
 
---
+++ app/Nova/Review.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\MorphTo;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Query\Search\SearchableMorphToRelation;
+use Laravel\Nova\Query\Search\SearchableText;
+
+class Review extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Review>
+     */
+    public static $model = \App\Models\Review::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+
+    public static $with = ['reviewable'];
+
+    public static $globalSearchResults = 10;
+
+    public function subtitle()
+    {
+        return match ($this->reviewable::class) {
+            \App\Models\Author::class => $this->reviewable->name,
+            \App\Models\Book::class => $this->reviewable->title,
+            default => null,
+        };
+    }
+
+    public static function searchableColumns()
+    {
+        return [
+          'id',
+          'title',
+          'stars',
+          new SearchableText('body'),
+          new SearchableMorphToRelation('reviewable', 'name', [Author::class]),
+          new SearchableMorphToRelation('reviewable', 'title', [Book::class]),
+      ];
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()
+                ->sortable(),
+
+            MorphTo::make('Reviewer')
+                ->types([
+                    User::class,
+                    Customer::class,
+                ]),
+
+            MorphTo::make('Reviewable')
+                ->types([
+                    Author::class,
+                    Book::class,
+                ]),
+
+            Text::make('Title')
+                ->rules('required', 'string', 'max:255'),
+
+            Trix::make('Body')
+                ->rules('required', 'string', 'max:65535'),
+
+            Number::make('Stars')
+                ->min(1)
+                ->max(5)
+                ->rules('nullable', 'integer', 'min:1', 'max:5'),
+
+            DateTime::make('Verified At')
+                ->nullable()
+                ->hideFromIndex()
+                ->rules('nullable', 'date'),
+
+            Boolean::make('Verified', fn () => $this->verified_at !== null)
+                ->filterable()
+                ->exceptOnForms(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
