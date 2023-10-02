3ddbbee237203de0eb4c4caee02aa2771e3aee2d
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-10-02 10:11:21 +0300

    24-Custom Tools (part 1 preparing) see Http::macro

---
+++ nova-components/NytBestsellers/resources/js/pages/Tool.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Head title="Nyt Bestsellers" />
+
+    <Heading class="mb-6">New York Times Bestsellers</Heading>
+
+    <Card>
+      {{ data.items }}
+    </Card>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, reactive } from "vue";
+
+const data = reactive({
+  items: [],
+});
+
+onMounted(() => {
+  Nova.request()
+    .get("/nova-vendor/nyt-bestsellers/")
+    .then((response) => {
+      data.items = response.data;
+    });
+});
+</script>
+
+<style>
+/* Scoped Styles */
+</style>
